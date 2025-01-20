7e952e8053d7831d762f7d587c5e6561f191b109
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2025-01-20 08:54:25 +0530

    fix: routing issue

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { jwtDecode } from "jwt-decode";
+
+const isTokenValid = (token: string | undefined): boolean => {
+    if (!token) return false;
+
+    try {
+        const decodedToken: { exp?: number } = jwtDecode(token);
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (decodedToken.exp && decodedToken.exp < currentTime) return false;
+        return true;
+    } catch {
+        return false;
+    }
+};
+
+export default function middleware(req: NextRequest) {
+    const { pathname, origin } = req.nextUrl;
+    const token = req.cookies.get("token")?.value;
+
+    if (pathname.startsWith("/_next") || pathname === "/favicon.ico") {
+        return NextResponse.next();
+    }
+
+    const isValidToken = isTokenValid(token);
+    if (!isValidToken && pathname !== "/") {
+        return NextResponse.redirect(new URL("/", origin));
+    }
+    if (isValidToken && pathname === "/") {
+        return NextResponse.redirect(new URL("/volunteer", origin));
+    }
+
+    return NextResponse.next();
+}
